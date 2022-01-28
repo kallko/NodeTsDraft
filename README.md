96d886307141803acfd9bfadf5a022897ee214c6
andriikalko <kallko_andrey@yahoo.co.uk>
2022-01-28 13:06:57 +0100

    11: pre send testing + README.md

---
+++ README.md
@@ -0,0 +1,38 @@
+Installation
+=============
+you should have installed global git and docker
+
+```
+mkdir Absence
+cd Absence
+git init
+git clone https://github.com/kallko/NodeTsDraft.git
+cd NodeTsDraft/
+docker-compose -p absence up
+```
+after this  (if everything ok) you could check in browser:
+http://localhost:5000/
+expected response like:
+```
+{"response":"main route success","time":"2022-01-28T11:55:02.771Z"}
+```
+By default, You will have in DB 1 user: 
+
+login: admin
+
+password admin
+
+and 3 quizes, created by admin.
+
+
+TESTING
+=============
+
+For testing You could use Postman
+
+step 1: registering
+-------
+```
+POST http://localhost:5000/register
+{"login": "admin2", "password": "123"}
+```
--- src/service/dbConnection.ts
+++ src/service/dbConnection.ts
@@ -1,11 +1,15 @@
 import { connect } from "mongoose";
 import { initData } from "./migrations/initData";
+import { UserSchema } from "./schema/user";
+import { userService } from "./userService";
 
 export async function setupMongoose(): Promise<void> {
   try {
-    const db = await connect("mongodb://172.20.0.1:27017/Quiz", {});
-    if (!db.modelNames().includes("User")) {
-      console.log("First start. Let add initial data.");
+    await connect("mongodb://172.20.0.1:27017/Quiz", {});
+    const userCount = (await userService.getNewIdForUser()) - 1;
+    console.log("User count ", userCount);
+    if (userCount === 0) {
+      console.log("First start. Let add initial data. ");
       await initData.addData();
     }
   } catch (err) {
