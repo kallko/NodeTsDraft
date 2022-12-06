4bfd102a64e3cff99df520c25aead08025f446ff
Brent Johnson <brent.johnson@casavi.de>
2022-12-06 10:29:22 +0100

    create getTop3Albums method signature

--- src/controller/dataController.ts
+++ src/controller/dataController.ts
@@ -1,5 +1,9 @@
+import { Rating, Album } from 'type';
 export const dataController = {
-  testFunction(input: number): any {
+/*   testFunction(input: number): any {
+    return input * 2;
+  }, */
+  getTop3Albums(ratings: Rating[], albums: Album[]) Album {
     return input * 2;
   },
 };
